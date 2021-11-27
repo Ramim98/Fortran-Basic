b1e319870eae8a8a7de297eba6017ce22334afa4
Ramim98 <63005807+Ramim98@users.noreply.github.com>
2021-11-27 15:58:56 +0600

    Add files via upload
    My first program in FORTRAN!

---
+++ Quadratic Equation.f90
@@ -0,0 +1,42 @@
+Program QuadEqu
+    implicit none
+    integer,parameter::rkind=selected_real_kind(p=7)
+    real(kind=rkind):: r1,r2,dt,a,b,c
+    complex::c1,c2,p
+    integer::x,n,i
+        print*, 'Enter the Value of a,b,c respectively : '
+    do
+
+
+    read*, a,b,c
+
+    dt=(b**2)-4*a*c
+
+    if (a==0) then
+        print*,'a can not be zero'
+
+        else if (dt<0) then
+        p=sqrt(cmplx(dt))
+        c1=(-b+p)/(2*a)
+        c2=(-b-p)/(2*a)
+
+        print*,'First root is: ' , c1
+
+        print*,'Second root is: ' , c2
+
+
+        else
+
+            r1=(-b+sqrt(dt))/(2*a)
+            r2=(-b-sqrt(dt))/(2*a)
+
+        print*,'First root is: ' , r1
+        print*,'Second root is: ' , r2
+
+    end if
+
+    print*, ' '
+  print*, 'Enter the Value of a,b,c respectively : '
+
+    end do
+End Program QuadEqu
